93970caa6646b4cfee5ed46484cd89187014f8b2
BarakBarzilay <barakoyb@gmail.com>
2022-07-11 15:56:24 +0300

    add feature responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, {
+  useState,
+  useEffect,
+} from 'react';
 import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
 function Header() {
+  const [backgroundMenu, setBackgroundMenu] =
+    useState(false);
+
+  const handleClick = () => {
+    setBackgroundMenu(!backgroundMenu);
+  };
+
+  const [screenWidth, setScreenWidth] = useState(
+    window.innerWidth
+  );
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener(
+      'resize',
+      changeWidth
+    );
+
+    return () => {
+      window.removeEventListener(
+        'resize',
+        changeWidth
+      );
+    };
+  }, []);
+
   return (
-    <header className='header'>
+    <header
+      className={
+        !backgroundMenu || screenWidth > 600
+          ? 'header'
+          : 'header header_theme_menu'
+      }>
       <h1 className='header__title'>
         NewsExplorer
       </h1>
-      <Navigation></Navigation>
+      <Navigation
+        screenWidth={screenWidth}
+        handleClick={handleClick}></Navigation>
     </header>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState,
+} from 'react';
 import './Navigation.css';
 import { Link } from 'react-router-dom';
 
-function Navigation() {
+function Navigation({
+  handleClick,
+  screenWidth,
+}) {
+  const [toggleMenu, setToggleMenu] =
+    useState(false);
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
   return (
     <nav className='nav'>
-      <a className='nav__link nav__link_active'>
-        Home
-      </a>
-      <a className='nav__link nav__signin'>
-        Sign in
-      </a>
+      {(toggleMenu || screenWidth < 600) && (
+        <button
+          onClick={() => {
+            toggleNav();
+            handleClick();
+          }}
+          className={
+            !toggleMenu
+              ? 'nav__menu-button'
+              : 'nav__menu-button nav__menu-button_close'
+          }></button>
+      )}
+      {(toggleMenu || screenWidth > 600) && (
+        <ul className='nav__list'>
+          <li className='nav__list-item'>
+            <a className='nav__link nav__link_active'>
+              Home
+            </a>
+          </li>
+          <li className='nav__list-item'>
+            <button className='nav__button'>
+              Sign in
+            </button>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
