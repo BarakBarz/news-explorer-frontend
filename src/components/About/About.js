ba1d868661473043d0aa40c9fa6934f8db416bc0
BarakBarzilay <barakoyb@gmail.com>
2022-08-01 13:45:18 +0300

    Capital letters at start of a sentence fix

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,27 +8,28 @@ function About() {
       <img src={authorPhoto} alt='Author' className='about__photo' />
       <div className='about__info'>
         <h3 className='about__header'>About the author</h3>
+
         <p className='about__description'>
           Hi, i'm Barak Barzilay but my friends call me Barak. I am a Web
           Developer that codes with MERN Stack tech, together with a good firm
-          grasp of Vanilla JS, HTML and CSS. i love to solve problems, and help
+          grasp of Vanilla JS, HTML and CSS. I love to solve problems, and help
           others become their better selves (another beautiful way to solve
-          issues). i also love working in groups, because i feel that everything
+          issues). I also love working in groups, because i feel that everything
           becomes more prolific, while working together towards one goal.
         </p>
         <p className='about__description'>
           I learned all this through Practicum Web Dev course. This Page here is
           my final project that incorporates everything i learned, together with
-          site deployment. in Practicum's, nine months course, we sprinted
-          through a lot of material. the course was simulated as a real job
+          site deployment. In Practicum's, nine months course, we sprinted
+          through a lot of material. The course was simulated as a real job
           environment, including deadlines, in order to get a feel of working in
           a tech company.
         </p>
         <p className='about__description'>
-          i am currently a senior student in practicum. this means that i am
+          I am currently a senior student in practicum. This means that i am
           helping other students work out their bugs in the code, and generally
           giving out tips that's related to the theory they're currently working
-          on. it also helps me stay on point, a learn even more.
+          on. It also helps me stay on point, and learn even more.
         </p>
       </div>
     </section>
