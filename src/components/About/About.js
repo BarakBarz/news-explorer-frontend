f7e5a3c37a9cd903e5545c142caf5cd2b9f39d17
Barak Barzilay <barakoyb@gmail.com>
2023-02-12 05:18:47 +0200

    change portrait. update description of author

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import authorPhoto from '../../images/portrait.svg';
+import authorPhoto from '../../images/portrait.jpg';
 import './About.css';
 
 function About() {
@@ -10,26 +10,25 @@ function About() {
         <h3 className='about__header'>About the author</h3>
 
         <p className='about__description'>
-          Hi, i'm Barak Barzilay but my friends call me Barak. I am a Web
-          Developer that codes with MERN Stack tech, together with a good firm
-          grasp of Vanilla JS, HTML and CSS. I love to solve problems, and help
-          others become their better selves (another beautiful way to solve
-          issues). I also love working in groups, because i feel that everything
-          becomes more prolific, while working together towards one goal.
+          Hi, i'm Barak Barzilay, Web Developer. With a found passion for
+          development, imaginative thinking, and a strong team orientation, I am
+          confident in my ability to make a positive impact on any development
+          project. I bring a positive mindset to every endeavor and firmly
+          believe in the benefits of collaboration, including fresh
+          perspectives, new ideas, and efficient problem-solving. I am eager to
+          bring my skills to a dynamic team environment.
         </p>
         <p className='about__description'>
-          I learned all this through Practicum Web Dev course. This Page here is
-          my final project that incorporates everything i learned, together with
-          site deployment. In Practicum's, nine months course, we sprinted
-          through a lot of material. The course was simulated as a real job
-          environment, including deadlines, in order to get a feel of working in
-          a tech company.
+          I learned all this through Masterschool and practicum's Web Dev
+          course. This web application here is my final project that
+          incorporates everything i've learned, together with site deployment.
+          In the nine months course, we sprinted through a lot of material. The
+          course was simulated in agile methodology, in order to get a feel of
+          working in a tech company.
         </p>
         <p className='about__description'>
-          I am currently a senior student in practicum. This means that i am
-          helping other students work out their bugs in the code, and generally
-          giving out tips that's related to the theory they're currently working
-          on. It also helps me stay on point, and learn even more.
+          I am currently learning more tech and how to implement it better and
+          staying up at night in bed thinking about bugs. 🪲
         </p>
       </div>
     </section>
